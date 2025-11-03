d3de9b43e63421cb6e1b93800c8145d35a79798f
ljm008jjang <ljm008jjang@naver.com>
2025-11-03 14:06:13 +0900

    Set a default value for ExecutionProvider in the ORTModel constructor

--- Runtime/Models/ORTModel.cs
+++ Runtime/Models/ORTModel.cs
@@ -85,11 +85,13 @@ namespace SparkTTS.Models
         /// <param name="modelName">The name of the model file (without extension)</param>
         /// <param name="modelFolder">The folder containing the model (from SparkTTSModelPaths)</param>
         /// <param name="precision">The precision of the model</param>
+        /// <param name="executionProvider">The execution provider for the model</param>
         protected ORTModel(
             string modelName, 
             string modelFolder, 
             bool preAllocateOutputs = false,
-            Precision precision = Precision.FP32)
+            Precision precision = Precision.FP32,
+            ExecutionProvider executionProvider = ExecutionProvider.CPU)
         {
             if (string.IsNullOrEmpty(modelName))
                 throw new ArgumentNullException(nameof(modelName));
@@ -100,6 +102,7 @@ namespace SparkTTS.Models
             {
                 ModelName = modelName,
                 Precision = precision,
+                ExecutionProvider = executionProvider,
                 ModelPath = Path.Combine(
                     Application.streamingAssetsPath,
                     SparkTTSModelPaths.BaseSparkTTSPathInStreamingAssets,
